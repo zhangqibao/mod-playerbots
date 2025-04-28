6a6e5b14e8337d4a07379718071de7a688603980
zhangqibao <zhangqibao@users.noreplay.github.com>
2025-04-29 01:17:48 +0800

    增加机器人名字汉化sql文件
    修改配置文件中文说明

---
+++ data/sql/characters/z2025_05_01_Stormworld_playerbots_arena_team_names.sql
@@ -0,0 +1,28 @@
+﻿DROP TABLE IF EXISTS `playerbots_arena_team_names`;
+CREATE TABLE `playerbots_arena_team_names` (
+                                               `name_id` mediumint(8) NOT NULL AUTO_INCREMENT UNIQUE,
+                                               `name` varchar(24) NOT NULL UNIQUE,
+                                               `type` TINYINT(3) NOT NULL,
+                                               PRIMARY KEY (`name_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Playerbot arena team names';
+
+
+DELETE FROM `playerbots_arena_team_names`;
+INSERT INTO `playerbots_arena_team_names` (`name_id`,`name`, `type`) VALUES
+(NULL, '觉醒战队',2),
+(NULL, '神族战队',2),
+(NULL, '不败战队',2),
+(NULL, '神族',3),
+(NULL, '电锯战队',3),
+(NULL, '精英部队',3),
+(NULL, '梦之队',5),
+(NULL, '逆袭',5),
+(NULL, '暗影战队',5),
+(NULL, '弑神战队',5),
+(NULL, '坑神大队',5),
+(NULL, '金色战队',5),
+(NULL, '极炫战队',5),
+(NULL, '娱乐战队',5),
+(NULL, '高富帅战队',5),
+(NULL, '瀚星云战队',5),
+(NULL, '切菜战队',5);
---
+++ data/sql/characters/z2025_05_01_Stormworld_playerbots_guild_names.sql
@@ -0,0 +1,18 @@
+﻿DROP TABLE IF EXISTS `playerbots_guild_names`;
+CREATE TABLE `playerbots_guild_names` (
+                                          `name_id` INT(11) NOT NULL AUTO_INCREMENT UNIQUE,
+                                          `name` varchar(24) NOT NULL UNIQUE,
+                                          PRIMARY KEY (`name_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Playerbot guild names';
+
+INSERT INTO `playerbots_guild_names` VALUES
+(NULL, '黑手'),
+(NULL, '永恒辉煌'),
+(NULL, '傲天神域'),
+(NULL, '雪域纵横'),
+(NULL, '燃烧军团'),
+(NULL, '红色自卫队'),
+(NULL, '锦衣卫'),
+(NULL, '基地组织'),
+(NULL, '楼兰月影');
+
